ab88139cedbe4dd852326649d9864b2c274484e4
Dávid Drobný <xdrobnyd@stuba.sk>
2020-04-10 18:34:03 +0200

    adding minimal Interface to teams

--- src/controllers/teamSceneController.java
+++ src/controllers/teamSceneController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import com.jfoenix.controls.JFXListCell;
+import com.jfoenix.controls.JFXListView;
 import javafx.fxml.FXML;
 
 public class teamSceneController extends userSceneController {
     @FXML
+    private JFXListView listView;
 
-    public
+    public void fillView() {
+    }
 }
