d47dc535b7133db5740850d4158f195af38b6f98
Dominik <dom.horvath17@gmail.com>
2020-04-28 13:38:33 +0200

    criminal detail added finished

--- src/controllers/criminalDetailController.java
+++ src/controllers/criminalDetailController.java
@@ -6,14 +6,17 @@ import datasource.ThreadCriminals;
 import entities.Criminal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,6 +35,9 @@ public class criminalDetailController implements Initializable {
     JFXProgressBar progressBar;
     @FXML
     private Button next,back;
+    @FXML
+    ContextMenu myMenuBar;
+
     int offset;
     int step = 16;
     String[] filter;
@@ -103,6 +109,20 @@ public class criminalDetailController implements Initializable {
             back.setDisable(true);
         ThreadCriminals threadCriminals = new ThreadCriminals(this.offset,this.filter);
         setUpTable(threadCriminals);
+    }
+    public void showDetail(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                "../scenes/criminalDetailScene.fxml"));
+        Criminal criminal =  tablePartners.getSelectionModel().getSelectedItem();
+        Parent root = (Parent) loader.load();
+        criminalDetailController ctrl = loader.getController();
+        ctrl.setCriminal(criminal);
+        ctrl.setDetails();
+        Scene scene2 = new Scene(root);
+        Stage window = (Stage) next.getScene().getWindow();
+        window.setTitle(criminal.getName() + " " + criminal.getSurname());
+        window.setScene(scene2);
+        window.show();
     }
 
 
